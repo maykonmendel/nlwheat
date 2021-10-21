889d0e1e28c3584e9211565598906c17639a0338
Maykon Mendel <maykonmendel.dev@gmail.com>
2021-10-21 15:46:00 -0300

    Autenticação do usuário recebendo o token

--- node_heat/src/services/AuthenticateUserService.ts
+++ node_heat/src/services/AuthenticateUserService.ts
@@ -63,7 +63,25 @@ class AuthenticateUserService {
             });
         }
 
-        return response.data;
+        const token = sign(
+            {
+                user: {
+                    name: user.name,
+                    avatar_url: user.avatar_url,
+                    id: user.id,
+                }
+            },
+
+            process.env.JWT_SECRET,
+
+            {
+                subject: user.id,
+                expiresIn: "1d"
+            }
+
+        );
+
+        return { token, user};
     }
 }
 
